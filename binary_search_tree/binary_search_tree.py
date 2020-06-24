7492409f5a9e5cc5721589e122904a82fa2f1ad2
Harry Merritt <harry@merritt.es>
2020-06-24 02:08:39 +0100

    BSTNode: fix insert function to pass tests

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -17,20 +17,17 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        if self.value is None:
-            self.value = value
-        else:
-            if self.value < value:
-                if self.right is None:
-                    self.right = BSTNode(value)
-                else:
-                    self.right.insert(value)
-
+        if self.value <= value:
+            if self.right is None:
+                self.right = BSTNode(value)
             else:
-                if self.left is None:
-                    self.left = BSTNode(value)
-                else:
-                    self.left.insert(value)
+                self.right.insert(value)
+
+        else:
+            if self.left is None:
+                self.left = BSTNode(value)
+            else:
+                self.left.insert(value)
 
     # Return True if the tree contains the value
     # False if it does not
